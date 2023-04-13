9506947f3592f125d3967f7f80deeafa08e10e13
forgetmenottt <129880361+forgetmenottt@users.noreply.github.com>
2023-04-13 23:47:31 +0500

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import numpy as np
+import scipy.stats as stats
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1372197133 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +12,28 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.04
+    conv_x = x_success/x_cnt #p246
+    conv_y = y_success/y_cnt #p247
+ 
+    # Пропорция успехов в комбинированном датасете:
+    conv_combined = (x_success + y_success)/(x_cnt + y_cnt)
+ 
+    # Разница конверсий/# разница пропорций в датасетах
+    p_diff = conv_x - conv_y
+ 
+    # считаем статистику в ст.отклонениях стандартного нормального распределения
+    z_value = p_diff / np.sqrt(conv_combined * (1 - conv_combined) * (1/x_cnt + 1/y_cnt))
+ 
+    # задаем стандартное нормальное распределение (среднее 0, ст.отклонение 1)
+    distr = stats.norm(0, 1)
+ 
+    p_value = (1 - distr.cdf(abs(z_value))) * 2
+ 
+ 
+ 
+    if (p_value < alpha):
+        a = True
+    else:
+        a = False
+    return a # Ваш ответ, True или False
